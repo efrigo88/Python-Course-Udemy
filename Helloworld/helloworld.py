7afce97b7e4132f9d5ddb29b42ce9b0f4e985b09
emif <emif_iron@hotmail.com>
2020-07-30 18:45:10 -0300

    ok

--- Helloworld/helloworld.py
+++ Helloworld/helloworld.py
@@ -3,3 +3,5 @@ print(string)
 
 calculation = 3 * 5
 print(calculation)
+
+print('first string', " --I'm concatenating this string", ' --now, I am adding this one')
---
+++ Helloworld/strings.py
@@ -0,0 +1,13 @@
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print('hello' + ' world')
+
+##
+greetings = 'Hello'
+name = input('Please enter your name: ')
+
+# if we want a space, we can add that too
+print(greetings + ' ' + name)
+
+
