b038d78e5b73ba836301e08da6be5ab2dc4ed0e0
emif <emif_iron@hotmail.com>
2020-07-30 21:39:37 -0300

    ok

--- Helloworld/escapechar.py
+++ Helloworld/escapechar.py
@@ -11,12 +11,19 @@ print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
 # or
 print("""The pet shop owner said "No, no, 'e's uh,...he's resting". """)
 
+#
 AnotherSplitString = """This line has been
-                  split over
-                  several
-                  times"""
+split over
+several
+times"""
 print(AnotherSplitString)
 
 # given exercise
+"""
 print('Number 1\tThe Larch')
-print('Number 2\tThe Horse Chestnut')
+print('Number 2\tThe Horse Chestnut'
+"""
+
+# interesting two ways of printing paths (the "r" is for raw strings)
+print("C:\\Users\\timbuchalka\\notes.txt")
+print(r"C:\Users\timbuchalka\notes.txt")
