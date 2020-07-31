6dba7a02349843b5a09d0d9281c43e63f9666cff
emif <emif_iron@hotmail.com>
2020-07-30 21:06:45 -0300

    ok

---
+++ Helloworld/escapechar.py
@@ -0,0 +1,22 @@
+string = 'I am going to try splitting this string now\nand I am going to do it again just now\nfunny stuff'
+print(string)
+
+tabbed_string = '1\t2\t3\t4\t5'
+print(tabbed_string)
+
+# two ways of escaping special characters
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+# or
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+# or
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting". """)
+
+AnotherSplitString = """This line has been
+                  split over
+                  several
+                  times"""
+print(AnotherSplitString)
+
+# given exercise
+print('Number 1\tThe Larch')
+print('Number 2\tThe Horse Chestnut')
