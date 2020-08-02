a6096127a1924a23f76591cfc7caa9de7c2dd37c
emif <emif_iron@hotmail.com>
2020-08-02 11:13:39 -0300

    ok

--- HelloWorld/strings2.py
+++ HelloWorld/strings2.py
@@ -9,7 +9,7 @@ for i in range(0, len(parrot)):
 
 print('\n')
 
-# mini challenge (I used the function find to get index for the letter, instead of counting manually)
+# mini challenge (I used the function 'find()' to get the letter's index, instead of counting manually)
 print(parrot)
 print(parrot[parrot.find('w')])
 print(parrot[parrot.find('e')])
@@ -18,3 +18,21 @@ print(parrot[parrot.find('w')])
 print(parrot[parrot.find('i')])
 print(parrot[parrot.find('n')])
 #
+
+# same mini challenge with negative indexes
+
+print(parrot)
+w = parrot.find('w') - 14
+e = parrot.find('e') - 14
+space = parrot.find(' ') - 14
+i = parrot.find('i') - 14
+n = parrot.find('n') - 14
+
+print('Negative indexes used:', w, e, space, i, n)
+
+print(parrot[w])
+print(parrot[e])
+print(parrot[space])
+print(parrot[w])
+print(parrot[i])
+print(parrot[n])
