faff02db1d2f15b75ec11bc80caa04219823a7f3
emif <emif_iron@hotmail.com>
2020-08-08 11:10:21 -0300

    ok

---
+++ ProgramFlow/blocks.py
@@ -0,0 +1,18 @@
+
+for i in range(1, 11):
+    print('No. {} squared is {} and cube is {}'.format(i, i ** 2, i ** 3))
+print('*' * 45)
+
+# indentation differences
+print()
+for i in range(1, 11):
+    print('No. {} squared is {} and cube is {}'.format(i, i ** 2, i ** 3))
+    print('*' * 45)
+
+
+
+
+
+
+
+
