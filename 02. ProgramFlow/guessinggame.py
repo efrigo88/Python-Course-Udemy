86d1c68d3179ac19e36c13bfb17f940550545667
emif <emif_iron@hotmail.com>
2020-08-09 12:10:10 -0300

    ok

---
+++ 02. ProgramFlow/guessinggame.py
@@ -0,0 +1,55 @@
+answer = 5
+
+print('Please guess a number between 1 and 10: ')
+guess = int(input())
+
+# redundant way of doing this
+# if guess < answer:
+#     print('Please guess higher')
+#     guess = int(input())
+#     if guess == answer:
+#         print('Well done, you guessed it')
+#     else:
+#         print('Sorry, you have not guessed correctly')
+# elif guess > answer:
+#     print('Please guess lower')
+#     guess = int(input())
+#     if guess == answer:
+#         print('Well done, you guessed it')
+#     else:
+#         print('Sorry, you have not guessed correctly')
+# else:
+#     print('You got it first time')
+
+# better way
+if guess != answer:
+    if guess < answer:
+        print('Please guess higher')
+    else:   # guess must be greater than answer
+        print('Please guess lower')
+    guess = int(input())
+    if guess == answer:
+        print('Well done, you guessed it')
+    else:
+        print('Sorry, you have not guessed correctly')
+else:
+    print('You got it first time')
+
+
+# challenge (first IF order reversed)
+if guess == answer:
+    print('You got it first time')
+else:
+    if guess < answer:
+        print('Please guess higher')
+    else:   # guess must be greater than answer
+        print('Please guess lower')
+    guess = int(input())
+    if guess == answer:
+        print('Well done, you guessed it')
+    else:
+        print('Sorry, you have not guessed correctly')
+
+
+
+
