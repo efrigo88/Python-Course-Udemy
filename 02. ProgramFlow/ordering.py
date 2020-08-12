d28181ad12c9b3ec30e3b2e32d28300aa0ef50c0
emif <emif_iron@hotmail.com>
2020-08-12 15:37:22 -0300

    ok

---
+++ 02. ProgramFlow/ordering.py
@@ -0,0 +1,24 @@
+# how to order a string alphabetically
+print()
+out_of_order = 'CBADEFGHIJKLZNOPQRSTUVWXYM'
+order_vector = sorted(out_of_order)
+result = ''
+
+for x in order_vector:
+    result = result + x
+print(result)
+
+# when working with strings, you should transform it to list to be able to exchange values into it
+chain = 'CBADEFGHIJKLMNOPQRSTUVWXYZ'
+chain_to_list = list(chain)
+
+x = chain_to_list[0]
+y = chain_to_list[1]
+z = chain_to_list[2]
+
+chain_to_list[0] = z
+chain_to_list[1] = y
+chain_to_list[2] = x
+
+print(chain_to_list)
+print(''.join(chain_to_list))
---
+++ 02. ProgramFlow/timestables.py
@@ -0,0 +1,13 @@
+for i in range(1, 13):
+    for j in range(1, 13):
+        print('{} times {} is {}'.format(j, i, j * i))
+    print('------------------')
+
+
+
+
+
+
+
+
+
