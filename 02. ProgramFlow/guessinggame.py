40432ba75f6b93abc88bc46bafe29666f88d8400
emif <emif_iron@hotmail.com>
2020-08-15 17:43:38 -0300

    ok

--- 02. ProgramFlow/guessinggame.py
+++ 02. ProgramFlow/guessinggame.py
@@ -1,42 +1,12 @@
-answer = 5
+import random
 
-print('Please guess a number between 1 and 10: ')
+highest = 10
+answer = random.randint(1, highest)
+print(answer)   # TODO: remember removing this line
+
+print('Please guess a number between 1 and {}: '.format(highest))
 guess = int(input())
 
-# redundant way of doing this
-# if guess < answer:
-#     print('Please guess higher')
-#     guess = int(input())
-#     if guess == answer:
-#         print('Well done, you guessed it')
-#     else:
-#         print('Sorry, you have not guessed correctly')
-# elif guess > answer:
-#     print('Please guess lower')
-#     guess = int(input())
-#     if guess == answer:
-#         print('Well done, you guessed it')
-#     else:
-#         print('Sorry, you have not guessed correctly')
-# else:
-#     print('You got it first time')
-
-# better way
-if guess != answer:
-    if guess < answer:
-        print('Please guess higher')
-    else:   # guess must be greater than answer
-        print('Please guess lower')
-    guess = int(input())
-    if guess == answer:
-        print('Well done, you guessed it')
-    else:
-        print('Sorry, you have not guessed correctly')
-else:
-    print('You got it first time')
-
-
-# challenge (first IF order reversed)
 if guess == answer:
     print('You got it first time')
 else:
@@ -49,6 +19,9 @@ else:
         print('Well done, you guessed it')
     else:
         print('Sorry, you have not guessed correctly')
+
+
+
 
 
 
---
+++ 02. ProgramFlow/whilechallenge.py
@@ -0,0 +1,34 @@
+import random
+
+print('Enter the number which will be the highest number to guess: ')
+highest = int(input())
+
+answer = random.randint(1, highest)
+n_of_guesses = 1
+
+print('Please guess a number between 1 and {}: '.format(highest))
+guess = int(input())
+
+while True:
+    if guess == answer:
+        print('Well done, you guessed it')
+        print('Number of guesses: {}'.format(n_of_guesses))
+        break
+    elif guess == 0:
+        print('You exited the program')
+        print('Number of guesses: {}'.format(n_of_guesses))
+        break
+    elif guess < answer:
+        print('Please guess higher')
+    else:
+        print('Please guess lower')
+
+    guess = int(input())
+    n_of_guesses += 1
+
+
+#   binary search logic
+#    low + (high - low) // 2
+
+
+
