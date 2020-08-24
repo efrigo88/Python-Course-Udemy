694fc4fe1a0ed88134182ea976003e10f3cc518d
emif <emif_iron@hotmail.com>
2020-08-24 16:56:16 -0300

    ok

--- 03. Sequences/mutable.py
+++ 03. Sequences/mutable.py
@@ -15,3 +15,18 @@ shopping_list += ['chocolate']
 print(shopping_list)
 # the id remained unchanged
 print(id(shopping_list))
+
+# we can see that this variable was updated as well with the new value
+# it's like a pointer (reference) in memory
+print()
+print(another_list)
+print(id(another_list))
+
+# another example
+print()
+a = b = c = d = e = f = another_list
+print('Adding Cream')
+b.append('cream')
+print(c)
+print(d)
+
