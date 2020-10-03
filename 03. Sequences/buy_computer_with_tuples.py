1e763a35b11bf85edadd2347fccb417ecb75ff5d
emif <emif_iron@hotmail.com>
2020-10-02 21:57:10 -0300

    ok

--- 03. Sequences/buy_computer_with_tuples.py
+++ 03. Sequences/buy_computer_with_tuples.py
@@ -9,29 +9,25 @@ available_parts = [
     ('HDMI Cable', 10),
 ]
 
+PART_NAME = 0
 current_choice = '-'
-computer_parts = []     # create an empty list
+computer_parts = []     # create an empty list to hold parts
 
 while current_choice != '0':
     if current_choice in '123456':
-        print('Adding {}'.format(current_choice))
-        if current_choice == '1':
-            computer_parts.append('Computer')
-        elif current_choice == '2':
-            computer_parts.append('Monitor')
-        elif current_choice == '3':
-            computer_parts.append('Keyboard')
-        elif current_choice == '4':
-            computer_parts.append('Mouse')
-        elif current_choice == '5':
-            computer_parts.append('Mouse Mat')
-        elif current_choice == '6':
-            computer_parts.append('HDMI Cable')
+        print('Adding option {}: {}'.format(current_choice, available_parts[int(current_choice) - 1][PART_NAME]))
+        computer_parts.append(available_parts[int(current_choice) - 1])
     else:
         print('Please add options from the list below:')
-        for i in available_parts:
-            print('{}: {}'.format(available_parts.index(i) + 1, i))
+        for index, (part_nm, part_price) in enumerate(available_parts):
+            print('Item Nº {}: {} (${})'.format(index + 1, part_nm, part_price))
         print('0: to finish')
     current_choice = input()
 
-print(computer_parts)
+price = 0
+print('You selected the following components:')
+for index, (part_nm, part_price) in enumerate(computer_parts):
+    print('{}: {}'.format(index + 1, part_nm))
+    price = price + part_price
+print('Price to pay: ${}'.format(price))
+
