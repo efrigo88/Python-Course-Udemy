a37b72a6f4f920bf4ca2e1a4e7f3909f1512b565
emif <emif_iron@hotmail.com>
2021-03-02 18:21:10 -0300

    ok

--- 07. Input&Output/_06_binary.py
+++ 07. Input&Output/_06_binary.py
@@ -1,14 +1,13 @@
 # we use 'bw' parameter, 'b' means binary, we are about to write in binary
 with open('writing/binary', 'bw') as bin_file:
     for i in range(17):
+        # a bytes-like object is required, that's why we pass the i
+        # between square brackets, to avoid passing an integer
         bin_file.write(bytes([i]))
 
 with open('writing/binary', 'br') as binFile:
     for i in binFile:
         print(i)
-
-
-
 
 
 
