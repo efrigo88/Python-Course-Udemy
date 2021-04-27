f2e8553e576ad8f9ec064316957306af2c4bc65e
EmilianoFrigoUala <emiliano.frigo@uala.com.ar>
2021-04-27 20:03:17 -0300

    ok

---
+++ 08. Modules and Functions/_03_browser.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+# webbrowser.open('https://www.python.org', new=1)
+
+# help(webbrowser)
+
+# for i in range(10):
+#     print(1, 2, 3, 4, 5, 6, 7, 8, 9, sep=';', end=' ')
+
+safari = webbrowser.get(using='safari')
+safari.open_new('https://www.python.org')
